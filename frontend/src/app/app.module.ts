4436c8a4efa7d2ba7538204f7b120375d3d5df1f
Juanara21 <110938196+Juanara21@users.noreply.github.com>
2023-03-26 12:36:27 -0500

    content

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -13,10 +13,10 @@ import { ContentNosotrosComponent } from './components/layout/content-nosotros/c
 import { Routes } from '@angular/router';
 
 const appRoutes: Routes = [
-{ path:'',component:ContentQuienesomosComponent}
-{ path:'',component:ContentQuienesomosComponent}
-{ path:'',component:ContentQuienesomosComponent}
-{ path:'',component:ContentQuienesomosComponent}
+{ path:'quienessomos',component:ContentQuienesomosComponent}
+{ path:'nosotros',component:ContentNosotrosComponent}
+{ path:'catologo',component:ContentCatalogoComponent}
+{ path:'',component:ContentComponent}
 
 
 ];
